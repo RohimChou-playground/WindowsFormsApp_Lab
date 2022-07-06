a2711b21169ba9462d7d8b5555129ed17735ff77
RohimChou <rohim.chou@gmail.com>
2022-07-06 09:15:40 +0800

    Add Drag From ListView To TextBox

---
+++ A02_DragDropListViewItemToTextBox/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿
+namespace WindowsFormsApp1 {
+	partial class Form1 {
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing) {
+			if (disposing && (components != null)) {
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent() {
+			this.listView1 = new System.Windows.Forms.ListView();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.SuspendLayout();
+			// 
+			// listView1
+			// 
+			this.listView1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+			this.listView1.HideSelection = false;
+			this.listView1.Location = new System.Drawing.Point(12, 12);
+			this.listView1.Name = "listView1";
+			this.listView1.Size = new System.Drawing.Size(334, 294);
+			this.listView1.TabIndex = 0;
+			this.listView1.UseCompatibleStateImageBehavior = false;
+			// 
+			// textBox1
+			// 
+			this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.textBox1.Location = new System.Drawing.Point(352, 12);
+			this.textBox1.Multiline = true;
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(334, 294);
+			this.textBox1.TabIndex = 1;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(698, 318);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.listView1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.ListView listView1;
+		private System.Windows.Forms.TextBox textBox1;
+	}
+}
+
---
+++ A02_DragDropListViewItemToTextBox/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1 {
+	public partial class Form1 : Form {
+		public Form1() {
+			InitializeComponent();
+
+			// Initialize ListView
+			this.listView1.View = View.Details;
+			this.listView1.FullRowSelect = true;
+			this.listView1.Font = new Font("Consolas", 10f);
+			this.listView1.Columns.Add(new ColumnHeader { Text = "Name", Width = 100 });
+			this.listView1.Columns.Add(new ColumnHeader { Text = "Size", Width = 80 });
+			this.listView1.Columns.Add(new ColumnHeader { Text = "Date modified", Width = 150 });
+
+			// add items
+			ListViewItem lvItem1 = this.listView1.Items.Add(key: "D:\\Dropbox", text: "Dropbox", imageKey: null);
+			lvItem1.SubItems.Add("100 MB");
+			lvItem1.SubItems.Add("5/17/2022 3:56 PM");
+			ListViewItem lvItem2 = this.listView1.Items .Add(key: "D:\\GoogleDrive", text: "GoogleDrive", imageKey: null);
+			lvItem2.SubItems.Add("59 MB");
+			lvItem2.SubItems.Add("5/01/2019 8:28 PM");
+			ListViewItem lvItem3 = this.listView1.Items.Add(key: "D:\\aaa.txt", text: "aaa.txt", imageKey: null);
+			lvItem3.SubItems.Add("15 KB");
+			lvItem3.SubItems.Add("5/16/2022 12:30 PM");
+			ListViewItem lvItem4 = this.listView1.Items.Add(key: "D:\\bbb.txt", text: "bbb.txt", imageKey: null);
+			lvItem4.SubItems.Add("1030 KB");
+			lvItem4.SubItems.Add("6/7/2022 02:00 PM");
+
+			// drag drop
+			this.listView1.AllowDrop = true;
+			this.listView1.DragEnter += new DragEventHandler(ListView1_DragEnter);
+			this.listView1.ItemDrag += new ItemDragEventHandler(ListView1_ItemDrag);
+
+			this.textBox1.AllowDrop = true;
+			this.textBox1.DragEnter += new DragEventHandler(TextBox1_DragEnter);
+			this.textBox1.DragDrop += new DragEventHandler(TextBox1_DragDrop);
+		}
+
+		private void ListView1_DragEnter(object sender, DragEventArgs e) {
+			if (e.Data.GetDataPresent(DataFormats.Text))
+				e.Effect = DragDropEffects.Copy;
+			else
+				e.Effect = DragDropEffects.None;
+		}
+
+		private void ListView1_ItemDrag(object sender, ItemDragEventArgs e) {
+			string itemText = ((ListViewItem)e.Item).Name;
+			DoDragDrop(itemText, DragDropEffects.Copy | DragDropEffects.Move);
+		}
+
+		private void TextBox1_DragEnter(object sender, DragEventArgs e) {
+			if (e.Data.GetDataPresent(DataFormats.Text)) { 
+				e.Effect = DragDropEffects.Copy;
+			} else { 
+				e.Effect = DragDropEffects.None;
+			}
+		}
+
+		private void TextBox1_DragDrop(object sender, DragEventArgs e) {
+			this.textBox1.Text += e.Data.GetData(DataFormats.Text)?.ToString() + Environment.NewLine;
+		}
+	}
+}
